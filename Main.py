11ee478963b99c149500bc99b653361d9d89c006
nicrodriguez <nrod123@live.com>
2018-01-16 02:16:40 -0600

    IO Redundancy
    Got rid of repetitive code

--- Main.py
+++ Main.py
@@ -1,7 +1,21 @@
-from RobustFit import robust_fit
+from PythonSimpleAndRobustFitting.RobustFit import robust_fit
 
 
-perfect_z_points = []
+def read_points_from_file(filename):
+    x_points = []
+    y_points = []
+    z_points = []
+    file = filename
+    with open(file, 'r') as f:
+        coordinates = f.readlines()
+        f.seek(0)
+        for point in coordinates:
+            points = point.replace("\n", "")
+            points = points.split(",")
+            x_points.append(int(points[0]))
+            y_points.append(int(points[1]))
+            z_points.append(int(points[2]))
+    return [x_points, y_points, z_points]
 
 
 def rms_error(z_points_c, z_points_a):
@@ -17,58 +31,41 @@ def rms_error(z_points_c, z_points_a):
     return 100*(abs(mean_error))**(1/2)
 
 
-def plane_to_point_rma(z_points_c, z_points_a):
+def plane_to_point_rms(z_points_c, z_points_a):
     print("Hello")
 
 
-# Reading from perfect plane file
-file = 'points_on_plane.txt'
-with open(file, 'r') as f:
-    coordinates = f.readlines()
-    f.seek(0)
-    # print(coordinates.sp)
-    for point in coordinates:
-        points = point.replace("\n", "")
-        points = points.split(",")
-        perfect_z_points.append(int(points[2]))
+vals = read_points_from_file('points_on_plane.txt')
+perfect_x_points = vals[0]
+perfect_y_points = vals[1]
+perfect_z_points = vals[2]
 
-# Reading from noisy plane file
-x_points = []
-y_points = []
-z_points = []
-file = 'noisy_points_on_plane.txt'
-with open(file, 'r') as f:
-    coordinates = f.readlines()
-    f.seek(0)
-    for point in coordinates:
-        points = point.replace("\n", "")
-        points = points.split(",")
-        x_points.append(int(points[0]))
-        y_points.append(int(points[1]))
-        z_points.append(int(points[2]))
+vals = read_points_from_file('noisy_points_on_plane.txt')
+x_points_n1 = vals[0]
+y_points_n1 = vals[1]
+z_points_n1 = vals[2]
 
-# Reading from noisy plane file
-x_points2 = []
-y_points2 = []
-z_points2 = []
-file = 'noisy_points_on_plane2.txt'
-with open(file, 'r') as f:
-    coordinates = f.readlines()
-    f.seek(0)
-    for point in coordinates:
-        points = point.replace("\n", "")
-        points = points.split(",")
-        x_points2.append(int(points[0]))
-        y_points2.append(int(points[1]))
-        z_points2.append(int(points[2]))
+vals = read_points_from_file('noisy_points_on_plane2.txt')
+x_points_n2 = vals[0]
+y_points_n2 = vals[1]
+z_points_n2 = vals[2]
 
+vals = read_points_from_file('noisy_points_on_plane3.txt')
+x_points_n3 = vals[0]
+y_points_n3 = vals[1]
+z_points_n3 = vals[2]
 
-robust_z_n1 = robust_fit(x_points, y_points, z_points)
+robust_z_n1 = robust_fit(x_points_n1, y_points_n1, z_points_n1)
 rms1 = rms_error(robust_z_n1, perfect_z_points)
 
-robust_z_n2 = robust_fit(x_points2, y_points2, z_points2)
+robust_z_n2 = robust_fit(x_points_n2, y_points_n2, z_points_n2)
 rms2 = rms_error(robust_z_n2, perfect_z_points)
+
+robust_z_n3 = robust_fit(x_points_n3, y_points_n3, z_points_n3)
+rms3 = rms_error(robust_z_n3, perfect_z_points)
 
 print("RMS Error with +/- 1 unit: {0:.4}%".format(rms1))
 print("RMS Error with +/- 2 unit: {0:.4}%".format(rms2))
+print("RMS Error with +/- 3 unit: {0:.4}%".format(rms3))
+
 
